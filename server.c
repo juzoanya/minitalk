4d45789d28dd0aa469bcfdcaa4662619b496a4fd
Jacob Uzoanya <juzoanya@student.42wolfsburg.de>
2022-02-24 19:16:05 +0100

    server update

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: juzoanya <juzoanya@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 17:15:12 by juzoanya          #+#    #+#             */
-/*   Updated: 2022/02/23 19:13:19 by juzoanya         ###   ########.fr       */
+/*   Updated: 2022/02/24 19:13:53 by juzoanya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,12 +35,24 @@ void	ft_putnbr_fd(int n, int fd)
 	write(fd, &n, 1);
 }
 
+void	msgrx(int signale, siginfo_t *siginfo, void *content)
+{
+	
+}
+
 int	main(void)
 {
 	int	pid;
+	struct sigaction	signale;
 
+	signale.sa_sigaction = msgrx;
+	signale.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &signale, NULL);
+	sigaction(SIGUSR2, &signale, NULL);
 	pid = getpid();
 	ft_putnbr_fd(pid, 1);
 	write(1, "\n", 1);
+	while (1)
+		pause();
 	return (0);
 }
