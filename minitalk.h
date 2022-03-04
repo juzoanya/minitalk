15141c1ac424b445502c3c00d5e2ec4cd102cb04
Jacob Uzoanya <juzoanya@student.42wolfsburg.de>
2022-03-04 20:00:30 +0100

    creation of util and .h files

--- minitalk.h
+++ minitalk.h
@@ -1,43 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   client.c                                           :+:      :+:    :+:   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juzoanya <juzoanya@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/23 17:14:58 by juzoanya          #+#    #+#             */
-/*   Updated: 2022/03/04 19:41:52 by juzoanya         ###   ########.fr       */
+/*   Created: 2022/03/04 19:01:56 by juzoanya          #+#    #+#             */
+/*   Updated: 2022/03/04 19:09:50 by juzoanya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
+#ifndef MINITALK_H
+# define MINITALK_H
 
-void	sig_tx(char *str, int pid)
-{
-	static int	bit;
-	static int	c;
+# include <unistd.h>
+# include <signal.h>
+# include <stdlib.h>
 
-	bit = -1;
-	if (bit < 0)
-	{
-		c = *str++;
-		if (!c)
-			exit(0);
-		bit = 7;
-	}
-	if ((c >> bit--) & 1)
-		kill(pid, SIGUSR1);
-	else
-		kill(pid, SIGUSR2);
-}
+void	ft_putnbr_fd(int n, int fd);
+int		ft_atoi(const char *nptr);
 
-int	main(int argc, char **argv)
-{
-	int	pid;
-
-	if (argc < 3 || !argv[2])
-		return (0);
-	pid = ft_atoi(argv[1]);
-	sig_tx(argv[2], pid);
-	return (0);
-}
+#endif
