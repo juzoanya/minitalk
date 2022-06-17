e1c21ea7b83161beb7600fa42d630925948505ac
Jacob Uzoanya <juzoanya@student.42wolfsburg.de>
2022-06-17 15:56:48 +0100

    final code

--- client.c
+++ client.c
@@ -6,33 +6,53 @@
 /*   By: juzoanya <juzoanya@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 17:14:58 by juzoanya          #+#    #+#             */
-/*   Updated: 2022/06/14 20:04:45 by juzoanya         ###   ########.fr       */
+/*   Updated: 2022/06/17 15:42:31 by juzoanya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	send_char(int id, char c)
+static void	close_conn(int pid)
+{
+	int	i;
+
+	i = 8;
+	while (i--)
+	{
+		usleep(100);
+		kill(pid, SIGUSR1);
+	}
+	exit(0);
+}
+
+static void	send_char(int id, char c)
 {
 	static int	pid;
 	static int	bit;
-	int			i;
 
-	if (id != -1)
+	if (id != 0)
 		pid = id;
-	i = 7;
-	while (i >= 0)
+	bit = 7;
+	while (bit >= 0)
 	{
-		if (((c >> i) & 1) == 1)
-			kill(pid, SIGUSR2);
+		if (!c)
+			close_conn(pid);
+		if (((c >> bit) & 1) == 1)
+		{
+			if (kill(pid, SIGUSR2) == -1)
+				err_handler(2);
+		}
 		else
-			kill(pid, SIGUSR1);
+		{
+			if (kill(pid, SIGUSR1) == -1)
+				err_handler(2);
+		}
 		usleep(100);
-		i--;
+		bit--;
 	}
 }
 
-void	send_str(int pid, char *str)
+static void	send_str(int pid, char *str)
 {
 	int	i;
 
@@ -45,10 +65,25 @@ void	send_str(int pid, char *str)
 	send_char(pid, str[i]);
 }
 
-void	sig_handler(int sig, siginfo_t *info, void *ucontext)
+static void	sig_handler(int sig, siginfo_t *info, void *ucontext)
 {
+	static int	cnt = 0;
+	static int	recv = 0;
+
 	(void)info;
 	(void)ucontext;
+	if (sig == SIGUSR1)
+	{
+		cnt++;
+		if (cnt == 8)
+		{
+			recv++;
+			cnt = 0;
+		}
+		write(1, "\r", 1);
+		ft_putnbr_fd(recv, 1);
+		ft_putstr_fd(" Bytes Recieved", 1);
+	}
 	if (sig == SIGUSR2)
 		send_char(0, 0);
 }
@@ -68,30 +103,7 @@ int	main(int argc, char *argv[])
 	if (sigaction(SIGUSR2, &signale, NULL))
 		err_handler(0);
 	send_str(pid, argv[2]);
+	while (1)
+		pause();
+	return (0);
 }
-
-//code with time check
-
-// #include <time.h>
-// #include <stdio.h>
-// int	main(int argc, char *argv[])
-// {
-// 	clock_t begin = clock();
-// 	double t_spent = 0.0;
-// 	struct sigaction	signale;
-// 	int					pid;
-
-// 	if (argc < 3)
-// 		err_handler(1);
-// 	pid = ft_atoi(argv[1]);
-// 	signale.sa_flags = SA_SIGINFO;
-// 	signale.sa_sigaction = sig_handler;
-// 	if (sigaction(SIGUSR1, &signale, NULL))
-// 		err_handler(0);
-// 	if (sigaction(SIGUSR2, &signale, NULL))
-// 		err_handler(0);
-// 	send_str(pid, argv[2]);
-// 	clock_t end = clock();
-// 	t_spent += (double)(end - begin) / CLOCKS_PER_SEC;
-// 	printf("time elapsed = %f\n", t_spent);
-// }
--- server.c
+++ server.c
@@ -6,13 +6,13 @@
 /*   By: juzoanya <juzoanya@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 17:14:58 by juzoanya          #+#    #+#             */
-/*   Updated: 2022/06/12 22:54:30 by juzoanya         ###   ########.fr       */
+/*   Updated: 2022/06/17 15:29:18 by juzoanya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int	get_bit(int bit, int pid)
+static int	get_bit(int bit, int pid)
 {
 	static char	c = 0;
 	static int	bits = 0;
@@ -35,19 +35,19 @@ int	get_bit(int bit, int pid)
 	return (kill(pid, SIGUSR1));
 }
 
-void	sig_handler(int sig, siginfo_t *siginfo, void *ucontext)
+static void	sig_handler(int sig, siginfo_t *siginfo, void *ucontext)
 {
-	int			bit;
+	int			bit_val;
 	static int	pid = 0;
 
 	(void)ucontext;
 	if (pid == 0)
 		pid = siginfo->si_pid;
 	if (sig == SIGUSR1)
-		bit = 0;
-	else
-		bit = 1;
-	if (get_bit(bit, pid))
+		bit_val = 0;
+	else if (sig == SIGUSR2)
+		bit_val = 1;
+	if (get_bit(bit_val, pid))
 		pid = 0;
 }
 
